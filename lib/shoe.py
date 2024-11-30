f7933b0ba1007be55c6d16a68ae471a22801c289
augustinemeso <augustinemesobk@gmail.com>
2024-11-30 15:34:15 +0300

    add updated PYTHON-P3-OOP-PUTTING-IT-TOGETHER

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,21 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = None  # Internal storage for size
+        self.size = size  # Set via the setter
+        self.condition = "New"  # Default condition
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        else:
+            self._size = value
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
